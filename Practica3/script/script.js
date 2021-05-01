c294d3bb0d57f3df27bc79ba5a6f6761edff204b
Alejandro Gonay <gonay_11@hotmail.com>
2021-04-30 23:23:36 -0500

    Primera parte
    Primera parte de practica 3

---
+++ Practica3/script/script.js
@@ -0,0 +1,10 @@
+
+function cnvs_getCoordinates(e){
+    x=e.clientX;
+    y=e.clientY;
+    document.getElementById("xycoordinates").innerHTML="Coordinates: (" + x + "," + y + ")";
+}
+
+function cnvs_clearCoordinates(){
+    document.getElementById("xycoordinates").innerHTML="";
+}
